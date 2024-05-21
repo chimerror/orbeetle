b43ff09f7a5017d10b5a43ddd0bc7529ebc01d7c
Jaycie Mitchell <chimerror@live.com>
2024-05-21 00:04:16 -0700

    CrawlCutscene: Adding ability to specify backgrounds
    Also changed formatting to spaces over tabs.

--- UI/Screens/CrawlCutscene.cs
+++ UI/Screens/CrawlCutscene.cs
@@ -1,42 +1,86 @@
 using Godot;
+using GColl = Godot.Collections;
 using GodotInk;
-using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 public partial class CrawlCutscene : Control
 {
-	private Control _crawlControl;
-	private Label _crawlLabel;
+    private TextureRect _backgroundTextureRect;
+    private Control _crawlControl;
+    private Label _crawlLabel;
+    private List<float> _orderedBackgroundPositions = new();
+    private readonly List<string> _orderedBackgroundNames = new();
+    private int _currentBackground = 0;
 
-	[Export]
-	public float CrawlSpeed { get; set; } = 0.9375f;
+    [Export]
+    public float CrawlSpeed { get; set; } = 0.9375f;
 
-	[Export]
-	public InkStory Story;
+    [Export]
+    public InkStory Story;
 
-	public override void _Ready()
-	{
-		_crawlControl = GetNode<Control>("%CrawlControl");
-		_crawlLabel = GetNode<Label>("%CrawlLabel");
+    [Export]
+    public GColl.Array<string> BackgroundNames { get; set; } = new();
 
-		_crawlLabel.Position = _crawlLabel.Position with { Y = Size.Y };
-		var builder = new StringBuilder();
-		while (Story.CanContinue)
-		{
-			builder.Append(Story.Continue());
-			builder.AppendLine();
-		}
-		_crawlLabel.Text = builder.ToString();
-	}
+    [Export]
+    public GColl.Array<Texture2D> Backgrounds { get; set; } = new();
 
-	public override void _Process(double delta)
-	{
-		var labelPosition = _crawlLabel.Position;
-		labelPosition.Y -= CrawlSpeed;
-		if (labelPosition.Y < -_crawlLabel.Size.Y)
-		{
-			labelPosition.Y = Size.Y;
-		}
-		_crawlLabel.Position = labelPosition;
-	}
+    private Dictionary<string, Texture2D> BackgroundDictionary
+    {
+        get => BackgroundNames.Zip(Backgrounds).ToDictionary(g => g.First, g => g.Second);
+    }
+
+    public override void _Ready()
+    {
+        _backgroundTextureRect = GetNode<TextureRect>("%BackgroundTextureRect");
+        _crawlControl = GetNode<Control>("%CrawlControl");
+        _crawlLabel = GetNode<Label>("%CrawlLabel");
+
+        _crawlLabel.Position = _crawlLabel.Position with { Y = Size.Y };
+        var backgroundCharacterPositions = new List<int>();
+        var builder = new StringBuilder();
+        while (Story.CanContinue)
+        {
+            var nextPart = Story.Continue();
+            if (nextPart.StartsWith("BACKGROUND "))
+            {
+                var backgroundName = nextPart.Replace("BACKGROUND ", string.Empty).TrimEnd();
+                backgroundCharacterPositions.Add(builder.Length);
+                _orderedBackgroundNames.Add(backgroundName);
+            }
+            else
+            {
+                builder.Append(nextPart);
+                builder.AppendLine();
+            }
+        }
+        var maxLength = (float)builder.Length;
+        _crawlLabel.Text = builder.ToString();
+
+        var crawlSize = _crawlLabel.Size.Y;
+        var screenMidpoint = Size.Y / 2.0f;
+        // TODO: I don't know why I needed to add that fudge factor of 1.5 here.
+        _orderedBackgroundPositions = backgroundCharacterPositions
+            .Select(p => screenMidpoint - p / maxLength * _crawlLabel.Size.Y * 1.5f)
+            .ToList();
+    }
+
+    public override void _Process(double delta)
+    {
+        var labelPosition = _crawlLabel.Position;
+        labelPosition.Y -= CrawlSpeed;
+
+        if (_currentBackground < _orderedBackgroundPositions.Count &&
+            labelPosition.Y < _orderedBackgroundPositions[_currentBackground])
+        {
+            _backgroundTextureRect.Texture = BackgroundDictionary[_orderedBackgroundNames[_currentBackground]];
+            _currentBackground++;
+        }
+        if (labelPosition.Y < -_crawlLabel.Size.Y)
+        {
+            labelPosition.Y = Size.Y;
+        }
+        _crawlLabel.Position = labelPosition;
+    }
 }
